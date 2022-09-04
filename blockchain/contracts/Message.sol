0a27f3efc61d33bdec1d6838386e576f974ae073
Oscar Serna <oscar.eduardo.serna@gmail.com>
2022-09-04 14:39:15 -0500

    inital commit

---
+++ blockchain/contracts/Message.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.0;
+
+
+import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract SenderMessage is  Ownable{
+
+    struct Message {
+        bytes cipherText;
+        uint ephempubkey;
+        address to;
+        address from;
+        uint value;
+    }
+
+    uint public totalSupply = 0;
+
+    mapping(address => uint) private balance;
+
+    mapping (uint => Message) message;
+    address authorized;
+
+    event Withdrawal(address indexed user, uint amount);
+    event NewMessage(address indexed from, address indexed to, uint indexed msgId, bytes text, uint pubkey, uint amount);
+
+
+
+    function setAuthorized(address _authorized) external onlyOwner{
+        authorized = _authorized;
+    }
+
+    function mint(bytes memory cipherText, uint ephempubkey, address to) external payable {
+        //id must never be 0
+        uint _id = totalSupply += 1;
+        // message[_id].cipherText = cipherText;
+        // message[_id].ephempubkey = ephempubkey;
+        // message[_id].from = _msgSender();
+        // message[_id].to = to;
+        // message[_id].value = msg.value;
+
+        //the NFT goes to the receiver
+        //super._mint(to, _id);
+        balance[to] += msg.value;
+        emit NewMessage(_msgSender(), to, _id, cipherText, ephempubkey, msg.value);
+        //return _id;
+    }
+
+    function getMessage(uint msgId) external view returns(Message memory){
+        return  message[msgId];
+    }
+
+    function withdraw(uint amount) external {
+        address user = _msgSender();
+        require(balance[user] >= amount, "Not enough balance");
+        balance[user] -= amount;
+        payable(user).transfer(amount);
+        emit Withdrawal(user, amount);
+    }
+
+    function userBalance(address user) external view returns(uint){
+        return balance[user];
+    }
+
+     
+   
+}
---
+++ blockchain/tests/profile_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import pytest
+from brownie import SenderMessage, accounts
+import brownie
+import random
+
+MESSAGES = ["This is a character 30 message",
+            "7 chars",
+            "This is a character 61 message This is a character 61 message",
+            ]
+
+
+#@pytest.fixture
+def test_message():
+    print(accounts[0])
+    print(type(accounts[0]))
+    print(int(str(accounts[0]),base=16))
+
+    message_contract = SenderMessage.deploy(
+            {'from': accounts[0], "gas_price": "auto"})
+
+    tx = message_contract.mint(bytes(MESSAGES[0],'utf-8'),0xffffffffffffff, accounts[1],
+                            {'from': accounts[0], "gas_price": "auto", 'value':5000000000000})
+    print(f"minting transaction from {accounts[0]} to {accounts[1]}: ",tx.info())
+
+
+    for i,msg in enumerate(MESSAGES):
+        print(msg)
+        tx = message_contract.mint(bytes(msg,'utf-8'),0xffffffffffffff, accounts[i+1],
+                            {'from': accounts[i], "gas_price": "auto", 'value':5000000000000})
+        print(f"minting transaction from {accounts[i]} to {accounts[i+1]}: ",tx.info())
+
+    for i,msg in enumerate(MESSAGES):
+        print(msg)
+        tx = message_contract.mint(bytes(msg,'utf-8'),0xffffffffffffff, accounts[i+1],
+                            {'from': accounts[i], "gas_price": "auto", 'value':5000000000000})
+        print(f"minting transaction from {accounts[i]} to {accounts[i+1]}: ",tx.info())
+        
+        
+    for i in range(1,5):
+        print(message_contract.getMessage(i))   
+    assert True == False
