5617a16e1b64e32c798d51387d7bb4ab3299905e
Sooki <0xSooki@gmail.com>
2022-09-06 00:25:35 +0200

    add tailwind, remove mui & restyle a bit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,39 @@
-import {Routes, Route, HashRouter} from 'react-router-dom'
-import './App.css';
-import Connect from './components/connect'
-import Home from './components/home'
-import getPubKey from "./logic/Ecnryption"
+import { Routes, Route, HashRouter } from "react-router-dom";
+import "./App.css";
+import Connect from "./components/connect";
+import Home from "./components/home";
+import getPubKey from "./logic/Ecnryption";
 import { ethers } from "ethers";
-import React, {useEffect, useState, useRef} from 'react'
+import React, { useEffect, useState, useRef } from "react";
 
-
-const contractABI = require("./abi/SenderMessage.json")
+const contractABI = require("./abi/SenderMessage.json");
 
 function App() {
-
-  const [provider,setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum));
+  const [provider, setProvider] = useState(
+    new ethers.providers.Web3Provider(window.ethereum)
+  );
   const [signer, setSigner] = useState(null);
-  const [myAddress, setMyAddress] = useState("")
+  const [myAddress, setMyAddress] = useState("");
   const messageABI = useRef(null);
 
-  useEffect( () => {
-  
-  const initContracts = async () => {
-      messageABI.current =  new ethers.Contract(
+  useEffect(() => {
+    const initContracts = async () => {
+      messageABI.current = new ethers.Contract(
         "0x11692A334351d4Be544Dc106B2447EBEdaac4A39",
         contractABI.abi,
         signer
-       );
-        
-      console.log("messageABI",messageABI);
-      console.log("signer",signer);
-    }
-      initContracts();
-      
-    },[provider,signer]);
-    
-      
+      );
+
+      console.log("messageABI", messageABI);
+      console.log("signer", signer);
+    };
+    initContracts();
+  }, [provider, signer]);
+
   const connectWallet = async () => {
-    const _provider = new ethers.providers.Web3Provider(window.ethereum)
+    const _provider = new ethers.providers.Web3Provider(window.ethereum);
     await _provider.send("eth_requestAccounts", []);
-    const signer =  _provider.getSigner();
+    const signer = _provider.getSigner();
     const _address = await signer.getAddress();
     setMyAddress(_address);
     console.log(_address);
@@ -45,27 +42,33 @@ function App() {
 
     getPubKey();
     return;
-  }
-
+  };
 
   return (
     <div>
       <HashRouter>
-        
         <Routes>
-          <Route exact path="/" 
-                 element={ 
-                    <Home signer={ signer } messageABI={ messageABI } myAddress={myAddress}
-                          provider={provider}
-                          contractAddress={"0x11692A334351d4Be544Dc106B2447EBEdaac4A39"}
-                        /> }
-                    />
-          <Route path="/connect"  element={<Connect connectWallet={connectWallet}/>} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Home
+                signer={signer}
+                messageABI={messageABI}
+                myAddress={myAddress}
+                provider={provider}
+                contractAddress={"0x11692A334351d4Be544Dc106B2447EBEdaac4A39"}
+              />
+            }
+          />
+          <Route
+            path="/connect"
+            element={<Connect connectWallet={connectWallet} />}
+          />
         </Routes>
       </HashRouter>
-      </div>
+    </div>
   );
-  
 }
 
 export default App;
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,121 +1,120 @@
-import React, { useEffect, useState } from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
-import hex_to_ascii from "../logic/helpers"
+import hex_to_ascii from "../logic/helpers";
 
 const CONTRACT_CREATION_BLOCK = 27933806;
 
-export default function Home(props){
+export default function Home(props) {
+  const navigate = useNavigate();
+  const [myMessages, setMyMessages] = useState([]);
 
-    const navigate = useNavigate();
-    const [myMessages, setMyMessages] = useState([])
+  useEffect(() => {
+    async function checkState() {
+      //await new Promise(r => setTimeout(r, 500));
+      if (props.signer === null) {
+        navigate("/connect");
+      }
+    }
+    checkState();
 
+    sync();
+    startListening();
+  }, [props.messageABI.current, props.signer]);
 
-   
+  const startListening = async () => {
+    if (props.messageABI.current !== null) {
+      while (props.messageABI.current.signer === null) {
+        console.log("waiting");
+        await new Promise((r) => setTimeout(r, 500));
+      }
+      props.messageABI.current.removeAllListeners();
+      props.messageABI.current.on(
+        "NewMessage",
+        (from, to, msgId, text, pubkey, amount) => {
+          if (to.toLowerCase() === props.myAddress.toLowerCase()) {
+            let info = {
+              from: from,
+              to: to,
+              msgId: msgId,
+              text: text,
+              pubkey: pubkey,
+              amount: amount,
+            };
+            console.log(info);
 
-    useEffect(( ) => {
-        async function checkState(){
-            //await new Promise(r => setTimeout(r, 500));
-            if(props.signer === null){
-                navigate('/connect');
+            // let _myMessages = [...myMessages];
+            // console.log("beofre pushing for real", _myMessages);
+            // _myMessages = [].concat(_myMessages, [{args:info}]);
+            // console.log("beofre pushing", _myMessages);
+            // _myMessages.push({args:info});
+            if (!myMessages.includes({ args: info })) {
+              setMyMessages((arr) => [...arr, { args: info }]);
+              setMyMessages((arr) => [...new Set(arr)]);
+              return;
             }
+          }
         }
-        checkState();
-        
-        sync();
-        startListening();
-    },[ props.messageABI.current, props.signer]);
+      );
+    }
+  };
 
-    const startListening = async () => {
-        if(props.messageABI.current !== null){
-            while (props.messageABI.current.signer === null){
-                console.log("waiting")
-                await new Promise(r => setTimeout(r, 500));
-            }
-            props.messageABI.current.removeAllListeners();
-            props.messageABI.current.on("NewMessage", (from, to, msgId, text, pubkey, amount) =>{
-                if (to.toLowerCase() === props.myAddress.toLowerCase()){
-                
-                let info = {
-                    from: from,
-                    to: to,
-                    msgId: msgId,
-                    text: text,
-                    pubkey: pubkey,
-                    amount: amount
-                }
-                console.log(info);
+  const sync = async () => {
+    let _myMessages = [];
+    if (props.messageABI.current !== null) {
+      while (props.messageABI.current.signer === null) {
+        console.log("waiting");
+        await new Promise((r) => setTimeout(r, 500));
+      }
+      props.messageABI.current.removeAllListeners();
+      console.log("syncing");
+      const lastBlock = await props.provider.getBlock();
+      const lastBlocknumber = lastBlock.number;
+      console.log("lastBlock", lastBlocknumber);
+      let i = CONTRACT_CREATION_BLOCK;
+      while (i < lastBlocknumber) {
+        if (props.messageABI.current.signer !== null) {
+          let end = i + 1000;
+          if (i + 1000 > lastBlocknumber) {
+            end = lastBlocknumber;
+          }
+          const allEvents = await props.messageABI.current.queryFilter(
+            "NewMessage",
+            i,
+            end
+          );
+          console.log(allEvents);
+          const filtered = allEvents.filter(
+            (_event) =>
+              _event.args[1].toLowerCase() === props.myAddress.toLowerCase()
+          );
+          console.log(filtered);
+          _myMessages = [].concat(_myMessages, filtered);
+          console.log(_myMessages);
+          setMyMessages(_myMessages);
+          i += 1000;
+        }
+      }
+      return;
+    }
+  };
 
-                // let _myMessages = [...myMessages];
-                // console.log("beofre pushing for real", _myMessages);
-                // _myMessages = [].concat(_myMessages, [{args:info}]);
-                // console.log("beofre pushing", _myMessages);
-                // _myMessages.push({args:info});
-                if (!myMessages.includes({args:info})){
-                    setMyMessages(arr => [...arr, {args:info}]);
-                    setMyMessages(arr => [...new Set(arr)]);
-                    return;
-                }
-                
-            }
-
+  console.log(props.signer);
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl">This is Home</h1>
+      <ul>
+        {myMessages
+          ? myMessages.map((message) => {
+              console.log(message);
+              return (
+                <p key={message.args.msgId}>
+                  {hex_to_ascii(message.args.text)}
+                </p>
+              );
             })
-        }
-    }
-    
-    const sync = async () => {
-        let _myMessages = [];
-        if(props.messageABI.current !== null){
-            while (props.messageABI.current.signer === null){
-                console.log("waiting")
-                await new Promise(r => setTimeout(r, 500));
-            }
-            props.messageABI.current.removeAllListeners();
-            console.log("syncing")
-            const lastBlock = await props.provider.getBlock();
-            const lastBlocknumber = lastBlock.number;
-            console.log("lastBlock", lastBlocknumber);
-            let i = CONTRACT_CREATION_BLOCK;
-            while(i < lastBlocknumber){
-                if(props.messageABI.current.signer !== null){
-                    let end = i +1000;
-                    if (i + 1000 > lastBlocknumber){
-                        end = lastBlocknumber;
-                    }
-                    const allEvents = await props.messageABI.current.queryFilter("NewMessage",i, end);
-                    console.log(allEvents);
-                    const filtered = allEvents.filter(_event => _event.args[1].toLowerCase() === props.myAddress.toLowerCase())
-                    console.log(filtered);
-                    _myMessages = [].concat(_myMessages, filtered);
-                    console.log(_myMessages);
-                    setMyMessages(_myMessages);
-                    i+=1000;
-                }
-            }
-            return;
-            
-        }
-        
-    }
-
-    console.log(props.signer);
-    return (
-
-      <div style={{height:"100vh",textAlign:"center",display:"block"}}>
-      
-        <h1>This is Home</h1>
-        <ul>
-            {myMessages?
-                myMessages.map( message => {
-                    console.log(message);
-                    return <p key={message.args.msgId}>{hex_to_ascii(message.args.text)}</p>
-                }):
-                null
-            }
-
-        </ul>
+          : null}
+      </ul>
     </div>
-        
-    )
+  );
 }
-
