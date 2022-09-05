2e6ea43cb4087927b7fce658eef81964cd637556
Oscar Serna <oscar.eduardo.serna@gmail.com>
2022-09-05 00:18:10 -0500

    syncing, listening and filtering message events per address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   const [provider,setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum));
   const [signer, setSigner] = useState(null);
+  const [myAddress, setMyAddress] = useState("")
   const messageABI = useRef(null);
 
   useEffect( () => {
@@ -34,7 +35,9 @@ function App() {
     const _provider = new ethers.providers.Web3Provider(window.ethereum)
     await _provider.send("eth_requestAccounts", []);
     const signer =  _provider.getSigner();
-    console.log(await signer.getAddress());
+    const _address = await signer.getAddress();
+    setMyAddress(_address);
+    console.log(_address);
     setProvider(_provider);
     setSigner(signer);
     return;
@@ -46,7 +49,13 @@ function App() {
       <HashRouter>
         
         <Routes>
-          <Route exact path="/" element={ <Home signer={ signer } messageABI={ messageABI }/> }/>
+          <Route exact path="/" 
+                 element={ 
+                    <Home signer={ signer } messageABI={ messageABI } myAddress={myAddress}
+                          provider={provider}
+                          contractAddress={"0x11692A334351d4Be544Dc106B2447EBEdaac4A39"}
+                        /> }
+                    />
           <Route path="/connect"  element={<Connect connectWallet={connectWallet}/>} />
         </Routes>
       </HashRouter>
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import { ethers } from "ethers";
+
+const CONTRACT_CREATION_BLOCK = 27933806;
 
 export default function Home(props){
 
     const navigate = useNavigate();
+    const [myMessages, setMyMessages] = useState([])
+
+
+   
 
     useEffect(( ) => {
         async function checkState(){
@@ -11,9 +18,44 @@ export default function Home(props){
             if(props.signer === null){
                 navigate('/connect');
             }
-        }checkState();
-    },[])
+        }
+        checkState();
+        
+        sync();
+    },[ props.messageABI.current, props.signer])
     
+    const sync = async () => {
+        let myMessages = []
+        if(props.messageABI.current !== null){
+            console.log("we got a valid contract")
+            while (props.messageABI.current.signer === null){
+                console.log("waiting")
+                await new Promise(r => setTimeout(r, 500));
+            }
+            const lastBlock = await props.provider.getBlock();
+            const lastBlocknumber = lastBlock.number;
+            console.log("lastBlock", lastBlocknumber);
+            let i = CONTRACT_CREATION_BLOCK;
+            while(i < lastBlocknumber){
+                if(props.messageABI.current.signer !== null){
+                    let end = i +1000;
+                    if (i + 1000 > lastBlocknumber){
+                        end = lastBlocknumber;
+                    }
+                    const allEvents = await props.messageABI.current.queryFilter("NewMessage",i, end);
+                    console.log(allEvents);
+                    const filtered = allEvents.filter(_event => _event.args[1].toLowerCase() === props.myAddress.toLowerCase())
+                    console.log(filtered);
+                    myMessages = [].concat(myMessages, filtered);
+                    console.log(myMessages);
+                    i+=1000;
+                }
+            }
+            
+            
+        }
+        
+    }
 
     console.log(props.signer);
     return (
@@ -21,6 +63,12 @@ export default function Home(props){
       <div style={{height:"100vh",textAlign:"center",display:"flex"}}>
       
         <h1>This is Home</h1>
+        {/* <ul>
+            {myMessages.map( message => {
+                <p key={message.id}>{message.text}</p>
+            })}
+
+        </ul> */}
     </div>
         
     )
